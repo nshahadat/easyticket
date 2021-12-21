c77ad3b60328c6e17164a28ae82b7246292421c9
beshahadat <shahadatnayeem23@gmail.com>
2021-12-22 00:19:08 +0600

    admin login page complete

--- admin-login.php
+++ admin-login.php
@@ -4,5 +4,39 @@ include "include/navbar.php";
 ?>
 
 <?php
+include "include/db_config.php";
+
+if(isset($_POST['ad_log_btn'])){
+    $uname = $_POST['ad_login_username'];
+    $pass = $_POST['ad_login_pwd'];
+
+    $sql = "SELECT * FROM $admin WHERE username = '$uname' AND password = '$pass'";
+
+    $result  = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+    $numrows = mysqli_num_rows($result);
+
+    if ($numrows == 0) {
+        echo "<script>alert('Wrong username or password')</script>";
+    } else {
+        session_start();
+        $_SESSION['name'] = $uname;
+
+        header("Location: admin-data-entry.php");
+
+    }
+}
+?>
+
+<div class="login-page" id="login">
+    <div class="form">
+        <form class="login-form" method="POST">
+            <input type="text" name="ad_login_username" placeholder="Admin Username"/>
+            <input type="password" name="ad_login_pwd"  placeholder="Admin Password"/>
+            <input type="submit" name="ad_log_btn"  value="Login" required/>
+        </form>
+    </div>
+</div>
+
+<?php
 include "include/footer.php";
 ?>
