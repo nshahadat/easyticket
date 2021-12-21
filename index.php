2a5405c53ef8b08268553933aa044e68abaf6caa
beshahadat <shahadatnayeem23@gmail.com>
2021-12-22 01:51:45 +0600

    index.php backend

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include "include/navbar.php";
 <p style="text-align:center; color:#5e8d93;">You must be registered and logged in to search and buy tickets form Easy Ticket.</p>
 <div class="login-page" id="login">
     <div class="form">
-        <form class="login-form">
+        <form class="login-form" method="POST">
             <input type="text" name="login_username" placeholder="username"/>
             <input type="password" name="login_pwd"  placeholder="password"/>
             <input type="submit" name="log_btn"  value="Login" required/>
@@ -16,7 +16,7 @@ include "include/navbar.php";
 </div>
 <div class="login-page" id="register">
     <div class="form">
-        <form class="login-form">
+        <form class="login-form" method="POST">
             <input type="text" name="reg_name"  placeholder="name" required/>
             <input type="text" name="reg_username"   placeholder="username" required/>
             <input type="email" name="reg_email"   placeholder="email" required/>
@@ -29,6 +29,56 @@ include "include/navbar.php";
         </form>
     </div>
 </div>
+
+<?php
+include "include/db_config.php";
+
+if (isset($_POST['log_btn'])){
+    $login_username = $_POST['login_username'];
+    $login_pwd = $_POST['login_pwd'];
+
+    $sql = "SELECT * FROM $user WHERE username = '$login_username' AND password = '$login_pwd'";
+
+    $result  = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
+    $numrows = mysqli_num_rows($result);
+
+    if ($numrows == 0) {
+        echo "<script>alert('Wrong username or password')</script>";
+    } else {
+        session_start();
+        $_SESSION['name'] = $login_username;
+
+        header("Location: searching.php");
+
+    }
+}
+
+if (isset($_POST['reg_btn'])){
+    $reg_name = $_POST['reg_name'];
+    $reg_username = $_POST['reg_username'];
+    $reg_email = $_POST['reg_email'];
+    $reg_contact = $_POST['reg_contact'];
+    $reg_address = $_POST['reg_address'];
+    $reg_pwd = $_POST['reg_pwd'];
+    $reg_con_pwd = $_POST['reg_con_pwd'];
+
+    if ($reg_pwd == $reg_con_pwd) {
+        $sql = "INSERT IGNORE INTO $user (name,email,username,contact,address,password) VALUES('$reg_name','$reg_email','$reg_username','$reg_contact','$reg_address','$reg_pwd')";
+
+        $mysqli->query($sql) or die($mysqli->error);
+        echo "<script>alert('You are registered succesfully. Login to continue.')</script>";
+
+        header("Location: index.php");
+    }
+    else{
+        echo "<script>alert('Insert accurate information')</script>";
+    }
+}
+else{
+    echo "<script>alert('Password and Confirm Password has to match.')</script>";
+}
+?>
+
 <?php
 include "include/footer.php";
 ?>
