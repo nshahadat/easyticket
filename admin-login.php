0917ebe4db5d66146c0a0c7092974b16c866f54e
beshahadat <shahadatnayeem23@gmail.com>
2021-12-22 21:01:15 +0600

    admin email

--- admin-login.php
+++ admin-login.php
@@ -9,8 +9,9 @@ include "include/db_config.php";
 if(isset($_POST['ad_log_btn'])){
     $uname = $_POST['ad_login_username'];
     $pass = $_POST['ad_login_pwd'];
+    $email = $_POST['ad_login_email'];
 
-    $sql = "SELECT * FROM $admin WHERE username = '$uname' AND password = '$pass'";
+    $sql = "SELECT * FROM $admin WHERE username = '$uname' AND password = '$pass' AND email = '$email'";
 
     $result  = mysqli_query($mysqli, $sql) or die(mysqli_error($mysqli));
     $numrows = mysqli_num_rows($result);
@@ -30,8 +31,9 @@ if(isset($_POST['ad_log_btn'])){
 <div class="login-page" id="login">
     <div class="form">
         <form class="login-form" method="POST">
-            <input type="text" name="ad_login_username" placeholder="Admin Username"/>
-            <input type="password" name="ad_login_pwd"  placeholder="Admin Password"/>
+            <input type="text" name="ad_login_username" placeholder="Admin Username" required/>
+            <input type="email" name="ad_login_email" placeholder="Admin Email" required/>
+            <input type="password" name="ad_login_pwd"  placeholder="Admin Password" required/>
             <input type="submit" name="ad_log_btn"  value="Login" required/>
         </form>
     </div>
