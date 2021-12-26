add0d3428cf2eaacac60ea0e72b3b3fff69d9374
beshahadat <shahadatnayeem23@gmail.com>
2021-12-26 20:09:25 +0600

    admin data entry backend

--- admin-data-entry.php
+++ admin-data-entry.php
@@ -4,9 +4,9 @@ include "include/navbar.php";
 ?>
 
 <div  style="text-align: right; padding-top: 30px; padding-right:20px;">
-    <h2>hello admin, &nbsp;<a href="admin-login.php" style="text-decoration:none !important;">
-    <input type="button" name="logout" value="Logout" style="width:fit-content !important;"></a>
-    </h2>
+    <p>hello admin, &nbsp;<a href="admin-login.php" style="text-decoration:none !important;">
+    <input type="button" name="logout" value="Logout" style="width:fit-content; background:none; color:#5e8d93 !important;"></a>
+</p>
 </div>
 
 <p style="text-align:center; color:#5e8d93;">Enter information.</p>
@@ -42,10 +42,50 @@ include "include/navbar.php";
             </div>
             <br>
             <input type="date" name="date_travel"  placeholder="Date"/>
-            <input type="submit" name="search_btn"  value="Search" required/>
+            <input type="time" name="time_travel" placeholder="Time"/>
+            <div class="custom-select">
+                <p style="text-align:center; color:#5e8d93;">Buses</p>
+                <select name="bus_name">
+                    <option value="Hanif Enterprise">Hanif Enterprise</option>
+                    <option value="Saintmartin Travels">SaintMartin Travels</option>
+                    <option value="Ena Paribahan">Ena Paribahan</option>
+                    <option value="Nabila Express">Nabila Express</option>
+                    <option value="Satkhira Paribahan">Satkhira Paribahan</option>
+                    <br><br>
+                </select>
+            </div>
+            <br>
+            <input type="number" name="price"  placeholder="Price"/>
+            <input type="number" name="avl_seats"  placeholder="Available Seats"/>
+            <br>
+            <input type="submit" name="entry_btn"  value="Entry" required/>
         </form>
     </div>
 </div>
+
+<?php
+
+include "include/db_config.php";
+
+if(isset($_POST['entry_btn'])){
+    $searching_from = $_POST['searching_from'];
+    $searching_to = $_POST['searching_to'];
+    $date_travel = $_POST['date_travel'];
+    $time_travel = $_POST['time_travel'];
+    $bus_name = $_POST['bus_name'];
+    $price = $_POST['price'];
+    $avl_seats = $_POST['avl_seats'];
+
+    $sql = "INSERT IGNORE INTO $ticket(from_location, to_location, date, bus, price, available_seats, time) VALUES('$searching_from','$searching_to','$date_travel','$bus_name','$price','$avl_seats','$time_travel')";
+
+    if ($insert = $mysqli->query($sql)) {
+        echo "<script>alert('Entry Succesfull')</script>";
+    } else {
+        echo "Something went wrong";
+    }
+}
+
+?>
 
 <?php
 include "include/footer.php";
